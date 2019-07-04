354ef202d3d8c10c87386abaaf25d2aa812af8fb
Adam Sherwin <adamsherwin@Adams-MBP.home>
2019-07-03 21:24:41 -0400

    Adding working image retrieval system. 'I' before 'E' except after c

--- server/app.js
+++ server/app.js
@@ -1,10 +1,53 @@
 const http = require('http');
 const renderview = require('./renderview.js');
+const readline = require('readline');
+const fs = require('fs');
 
 const port = 40001;
+const configFileArgNum = 2; // With respect to 0 index
 
-const server = http.createServer((req, res) => {
-    renderview.process(req, res);
-}).listen(port, () => {
-    console.log('Server running');
-});
+if (process.argv.length < configFileArgNum + 1)
+{
+    console.log("ERROR: Incorrect number of arguments supplied");
+    console.log("Usage: node app.js [config/file/name]");
+} // if
+else
+{
+    // Configuration file name from command line arguments
+    var configFile = process.argv[configFileArgNum]
+
+    // parseConfigFile - reads config file and returns map parameter->value
+    function parseConfigFile(configFileName)
+    {
+        // Array to convert to map
+        var configArray = [];
+        var lines = fs.readFileSync(configFileName, 'utf-8').split('\n');
+        for (var index = 0; index < lines.length; ++index)
+        {
+            if (lines[index].includes('#') == false)
+            {
+                if (lines[index].includes('=') == true)
+                {
+                    var splitLine = lines[index].split('=');
+                    if (splitLine.length == 2)
+                    {
+                        configArray.push([splitLine[0], splitLine[1]]);
+                    } // if
+                } // if
+            } // if
+        } // for
+        var map = new Map(configArray);
+        return map;
+    } // parseConfigFile
+
+    var config = parseConfigFile(configFile);
+
+    var imageFileDirectory = config.get('ImageSaveDirectory');
+    renderview.SetImageFileDirectory(imageFileDirectory);
+
+    const server = http.createServer((req, res) => {
+        renderview.process(req, res);
+    }).listen(port, () => {
+        console.log('Server running');
+    });
+} // else
--- server/renderview.js
+++ server/renderview.js
@@ -1,11 +1,72 @@
-var commonHeader = { 'Content-Type': 'html' };
+// Requires
+var fs = require("fs");
+var path = require('path');
+
+// Variables
+var htmlHeader = { 'Content-Type': 'html' };
+var jpgHeader = { 'Content-Type': 'image/jpg' }
+var mImageFileDirectory = "."
+
+// mergeValues - replace string content with values from key/value Object
+function mergeValues(values, content) {
+  for (var key in values) {
+    var reg = new RegExp("{{" + key + "}}", "g");
+    content = content.replace(reg, values[key])
+  }
+  return content;
+} // mergeValues
+
+function SetImageFileDirectory(imageFileDirectory)
+{
+    mImageFileDirectory = imageFileDirectory
+} // SetImageFileDirectory
+
+// view - get templateName.html,
+function view(templateName, values, res) {
+  var fileContent = fs.readFileSync('./views/' + templateName + '.html', 'utf8');
+  fileContent = mergeValues(values, fileContent);
+  res.write(fileContent);
+} // view
 
 // process - append output to res, and return to calling function
-function process()
+function process(req, res)
 {
-    res.writeHead(200, commonHeader);
-} // process
+    if (req.url === "/")
+    {
+        res.writeHead(200, htmlHeader);
+        view('header', {}, res);
+        view('intro', {}, res);
 
+        // Read files in image directory
+        var files = fs.readdirSync(mImageFileDirectory);
+        //listing all files using forEach
+
+        for (var i = 0; i < files.length; ++i)
+        {
+            var timestampString = files[i].split('.');
+            var timestampIntMillis = parseInt(timestampString) * 1000;
+            var dateRepresentation = new Date(timestampIntMillis);
+
+            view('detection', {'imagename': dateRepresentation, 'imagelink': mImageFileDirectory + "/" + files[i]}, res);
+        } // for
+
+        view('footer', {}, res);
+        res.end();
+    } // if
+    else if (req.url.match("\.jpg"))
+    {
+        var imagePath = req.url;
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, jpgHeader);
+        fileStream.pipe(res);
+    } // if
+    else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    } // else
+
+} // process
 
 // API
 module.exports.process = process
+module.exports.SetImageFileDirectory = SetImageFileDirectory
